015afe9f58c23e86a0a4f9b1920d52f9a129798d
Duane Felton <dfelton2010@gmail.com>
2018-09-18 17:10:30 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -6,8 +6,10 @@ def oxford_comma(array)
   elsif array.length == 2
     array.join(" and ")
   
-  elsif array.length == 3
+  elsif array.length >= 3
     return "#{array[0..-2].join(", ")}, and #{array.last}"
+    
+  
     
 end
 end
